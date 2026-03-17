fc04c37740d1c9b8b70b15aeeb2e9ea6a2c3a9ec
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2026-03-17 12:21:11 +0100

    SEO modifications

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://javipiqueras.dev',
+  integrations: [sitemap()],
   adapter: vercel(),
   i18n: {
     defaultLocale: 'en',
